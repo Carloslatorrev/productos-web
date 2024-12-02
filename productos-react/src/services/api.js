3564c981ceb60c3adcf079d7cd92d237acee2511
Carloslatorrev <63814415+Carloslatorrev@users.noreply.github.com>
2024-12-02 00:57:55 -0300

    Update api.js

--- productos-react/src/services/api.js
+++ productos-react/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api'; 
+const API_URL = 'https://productoapp-f3cmatc6gfhzawg3.canadacentral-01.azurewebsites.net/api'; 
 
 const api = axios.create({
   baseURL: API_URL,
